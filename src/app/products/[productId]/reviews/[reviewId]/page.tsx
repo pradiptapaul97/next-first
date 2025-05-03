310521b14689f605032e51237639a4841d5738cf
Pradipta Paul <pradipta.paul@webskitters.com>
2025-05-03 23:18:57 +0530

    Navigating Programmatically

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,11 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 export default async function ReviewDetails({ params }: { params: Promise<{ productId: string, reviewId: string }> }) {
     const reviewId = (await params).reviewId;
     const productId = (await params).productId;
 
     if (Number(reviewId) > 1000) {
-        notFound();
+        // notFound();
+        redirect("/products")
     }
     return (
         <>
